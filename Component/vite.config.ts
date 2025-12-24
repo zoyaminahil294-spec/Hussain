d883338e53665ef3dfe561b424c03e830af7c34b
mudasser376897 <zoyaminahil294@gmail.com>
2025-12-24 14:46:54 +0500

    Add files via upload

---
+++ Component/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+  }
+});
